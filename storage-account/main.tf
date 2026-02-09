b0ef538e9a5dd833db0bf5cb3a069ddbe80be5fc
anisha2902 <154617956+anisha2902@users.noreply.github.com>
2026-02-09 13:49:03 +0530

    Update main.tf

--- storage-account/main.tf
+++ storage-account/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "${var.name}sa"
+  name                     = "${var.name}demosa"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = var.account_tier
